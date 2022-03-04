6e0b303682a202af460fd75e1525f65886936485
Chimoha31 <mokochii1108@gmail.com>
2022-03-03 23:19:32 -0800

    Done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
 import db from "./components/firebase";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, onSnapshot } from "firebase/firestore";
 
 function App() {
   // dbの内容をpostの中にどんどん入れていく為にuseState()で箱を用意する。
@@ -14,6 +14,11 @@ function App() {
       //  console.log(snapShot.docs.map((doc) => ({...doc.data()})));
       setPosts(snapShot.docs.map((doc) => ({ ...doc.data() })));
     });
+
+    // リアルタイムでデータを取得する↓
+    onSnapshot(postData, (post) => {
+      setPosts(post.docs.map((doc) => ({...doc.data()})))
+    })
   }, []);
 
   return (
